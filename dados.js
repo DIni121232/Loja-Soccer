2b1c4e98aa65bb705f80ef781a5eb3927f854df3
DIni121232 <giankelve403@gmail.com>
2025-03-25 17:41:33 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,60 @@
+let dados = [
+    {   nome : "Nike",descricao : "",
+        
+        link : "https://www.nike.com.br/bola-nike-cbf-strike-society-028436.html?cor=51"
+    },
+    {
+        nome : "Adidas",
+        descricao : "",
+        link : "https://www.adidas.com.br/bolas"
+    },
+    {
+        nome : "Topper",
+        descricao : "",
+        link : "https://www.topper.com.br/bolas?mi=hm_ger_mntop_bolas&psn=Menu_Top"
+    },
+    {
+        nome : "Puma",
+        descricao : "",
+        link : "https://br.puma.com/bola-puma-prestige-084585-02.html?_sgm_campaign=product&_sgm_source=084585_02&_sgm_action=search&_sgm_term=bola&_sgm_pinned=false&tamanho=5"
+    },
+    {
+        nome : "Umbro",
+        descricao : "",
+        link : "https://www.umbro.com.br/bola-de-futsal-umbro-sala-v-pro-1217835/p"
+    },
+    {
+        nome : "Penalty",
+        descricao : "",
+        link : "https://www.penalty.com.br/bolas"
+    },
+    {
+        nome : "Mitre",
+        descricao : "",
+        link : ""
+    },
+    {
+        nome : "Molten",
+        descricao : "",
+        link : "https://www.molten.com.br/"
+    },
+    {
+        nome : "Wilson",
+        descricao : "",
+        link : "https://www.wilsonloja.com.br/collections/bolas-basquete"
+    },
+    {
+        nome : "Spalding",
+        descricao : "",
+        link : "https://www.spaldingbrasil.com.br/"
+    },
+    {
+        nome : "Select",
+        descricao : "",
+        link : ""
+    }
+    
+    
+
+    
+];
